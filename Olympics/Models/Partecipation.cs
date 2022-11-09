a46f648773104a302920271b89d9a828b2dd05b1
bibadada <davideregnani@gmail.com>
2022-11-09 17:54:00 +0100

    Aggiungere i file di progetto.

---
+++ Olympics/Models/Partecipation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympics.Models
+{
+    class Partecipation
+    {
+        public long Id { get; set; }
+        public long? IdAthlete { get; set; }
+        public string Name { get; set; }
+        public char Sex { get; set; }
+        public int? Age { get; set; }
+        public int? Height { get; set; }
+        public int? Weight { get; set; }
+        public string NOC { get; set; }
+        public string Games { get; set; }
+        public int? Year { get; set; }
+        public string Season { get; set; }
+        public string City { get; set; }
+        public string Sport { get; set; }
+        public string Event { get; set; }
+        public string Medal { get; set; }
+
+    }
+}
